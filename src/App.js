d1236d0cd23f2d756d703f41570985a8b4282640
Fadi Alsaidi <falsa084@uottawa.ca>
2024-07-17 13:42:36 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
